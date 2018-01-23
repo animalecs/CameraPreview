b91f010284dda43771e887dababa22517928725c
DESKTOP-61T0PRU\Alex <alexmina96@gmail.com>
2018-01-24 00:35:22 +0100

    Added the possibility of taking a photo and saving it Changed the photo button Added the rotation animation also to the switch camera icon

--- CameraPreview/app/src/main/java/com/alex/camerapreview/MainActivity.java
+++ CameraPreview/app/src/main/java/com/alex/camerapreview/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.graphics.SurfaceTexture;
 import android.hardware.SensorManager;
 import android.hardware.camera2.CameraAccessException;
@@ -14,6 +15,7 @@ import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.v4.app.ActivityCompat;
@@ -25,12 +27,20 @@ import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.alex.camerapreview.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends Activity {
 
@@ -50,10 +60,12 @@ public class MainActivity extends Activity {
     CameraCaptureSession cameraCaptureSession;
     ImageView imgFlashLight, imgChangeCamera;
     OrientationEventListener mOrientationListener;
+    RelativeLayout frameButton;
+    File galleryFolder;
     public enum Rotation {
         PORTRAIT, LANDSCAPE, REVERSE_LANDSCAPE
     }
-    Rotation imgFlashlightRotation = Rotation.PORTRAIT;
+    Rotation currentRotation = Rotation.PORTRAIT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +73,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        //ask for permission for camera and storage in order to save pics
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE}, CAMERA_REQUEST_CODE);
 
         cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         //start with the back camera
@@ -69,7 +83,9 @@ public class MainActivity extends Activity {
         textureView = (TextureView)findViewById(R.id.textureView);
         imgFlashLight = (ImageView)findViewById(R.id.imgFlashlight);
         imgChangeCamera = (ImageView)findViewById(R.id.imgChangeCamera);
+        frameButton = (RelativeLayout)findViewById(R.id.frameLayout);
 
+        //orientation listener used to rotate the icons when device is in landscape mode
         mOrientationListener = new OrientationEventListener(this,
                 SensorManager.SENSOR_DELAY_NORMAL) {
 
@@ -81,17 +97,19 @@ public class MainActivity extends Activity {
 
                 if (orientation < 45 || orientation > 315) {
                     //0 degrees
-                    if(imgFlashlightRotation != Rotation.PORTRAIT){
-                        imgFlashlightRotation = Rotation.PORTRAIT;
+                    if(currentRotation != Rotation.PORTRAIT){
+                        currentRotation = Rotation.PORTRAIT;
                         imgFlashLight.animate().rotation(0).start();
+                        imgChangeCamera.animate().rotation(0).start();
                     }
 
                 }
                 else if (orientation < 135) {
                     //90 degrees
-                    if(imgFlashlightRotation != Rotation.LANDSCAPE){
-                        imgFlashlightRotation = Rotation.LANDSCAPE;
+                    if(currentRotation != Rotation.LANDSCAPE){
+                        currentRotation = Rotation.LANDSCAPE;
                         imgFlashLight.animate().rotation(-90).start();
+                        imgChangeCamera.animate().rotation(-90).start();
                     }
                 }
                 else if (orientation < 225) {
@@ -100,9 +118,10 @@ public class MainActivity extends Activity {
                 }
                 else
                 {
-                    if(imgFlashlightRotation != Rotation.REVERSE_LANDSCAPE){
-                        imgFlashlightRotation = Rotation.REVERSE_LANDSCAPE;
+                    if(currentRotation != Rotation.REVERSE_LANDSCAPE){
+                        currentRotation = Rotation.REVERSE_LANDSCAPE;
                         imgFlashLight.animate().rotation(90).start();
+                        imgChangeCamera.animate().rotation(90).start();
                     }
                 }
 
@@ -117,6 +136,35 @@ public class MainActivity extends Activity {
         }
 
 
+        frameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ///stop shooting while taking the pic
+                lock();
+                FileOutputStream outputPhoto = null;
+                try {
+                    //create the folder for the image if it's not already existing
+                    createImageGallery();
+                    outputPhoto = new FileOutputStream(createImageFile(galleryFolder));
+                    textureView.getBitmap()
+                            .compress(Bitmap.CompressFormat.PNG, 100, outputPhoto);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    //start shooting again
+                    unlock();
+                    try {
+                        if (outputPhoto != null) {
+                            outputPhoto.close();
+
+                            Toast.makeText(MainActivity.this, "Image taken", Toast.LENGTH_LONG).show();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
         imgChangeCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -181,20 +229,27 @@ public class MainActivity extends Activity {
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //open the background thread that handles the camera
-        openBackgroundThread();
-        //if the textureview is already available just set it up
-        if (textureView.isAvailable()) {
-            setUpCamera();
-            openCamera();
-        } else {
-            //attach the listener to the textureview
-            textureView.setSurfaceTextureListener(surfaceTextureListener);
+
+    private void lock() {
+        try {
+            //submit a request for an image to be captured by the camera device
+            cameraCaptureSession.capture(captureRequestBuilder.build(),
+                    null, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
         }
     }
+
+    private void unlock() {
+        try {
+            cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(),
+                    null, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 
     private void createPreviewSession() {
@@ -210,9 +265,8 @@ public class MainActivity extends Activity {
 
                         @Override
                         public void onConfigured(CameraCaptureSession cameraCaptureSession) {
-                            if (cameraDevice == null) {
-                                return;
-                            }
+                            if (cameraDevice == null)  return;
+
 
                             try {
                                 captureRequest = captureRequestBuilder.build();
@@ -231,6 +285,21 @@ public class MainActivity extends Activity {
                     }, backgroundHandler);
         } catch (CameraAccessException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //open the background thread that handles the camera
+        openBackgroundThread();
+        //if the textureview is already available just set it up
+        if (textureView.isAvailable()) {
+            setUpCamera();
+            openCamera();
+        } else {
+            //attach the listener to the textureview
+            textureView.setSurfaceTextureListener(surfaceTextureListener);
         }
     }
 
@@ -280,6 +349,25 @@ public class MainActivity extends Activity {
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    private void createImageGallery() {
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        galleryFolder = new File(storageDirectory, getResources().getString(R.string.app_name));
+        if (!galleryFolder.exists()) {
+            boolean wasCreated = galleryFolder.mkdirs();
+            if (!wasCreated) {
+                //manage the error
+
+                Toast.makeText(this, "Error while creating folder", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private File createImageFile(File galleryFolder) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "image_" + timeStamp + "_";
+        return File.createTempFile(imageFileName, ".jpg", galleryFolder);
     }
 
 
